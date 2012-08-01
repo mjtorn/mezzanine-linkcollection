839f3df5357d1380c1f36957b26d726078be36c4
Markus Törnqvist <mjt@fadconsulting.com>
2012-08-01 21:15:39 +0300

    admin

---
+++ mezzalinkcollection/admin.py
@@ -0,0 +1,28 @@
+# vim: tabstop=4 expandtab autoindent shiftwidth=4 fileencoding=utf-8
+
+from django.contrib import admin
+
+from . import models
+
+class LinkAdmin(admin.ModelAdmin):
+    """Custom admin
+    """
+
+    fields = ('url', 'title', 'description', 'gen_description', 'added_by')
+
+    def get_form(self, request, obj=None, **kwargs):
+        """We do not want to autogenerate the description here
+        """
+
+        form = super(LinkAdmin, self).get_form(request, obj=obj, **kwargs)
+
+        ## FIXME: does not seem to work, maybe we got instantiated? investigate later
+        form.base_fields['gen_description'].default = False
+        form.base_fields['added_by'].default = request.user
+
+        return form
+
+admin.site.register(models.Link, LinkAdmin)
+
+# EOF
+
