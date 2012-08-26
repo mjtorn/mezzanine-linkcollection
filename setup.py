43ad51c010d0dce7eb9711e68261009ca923c400
Markus Törnqvist <mjt@fadconsulting.com>
2012-08-26 15:15:03 +0300

    Versionbump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 NAME = 'mezzanine-linkcollection'
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 
 DESCR = """\
 A fairly basic model for maintaining a link list on your site
